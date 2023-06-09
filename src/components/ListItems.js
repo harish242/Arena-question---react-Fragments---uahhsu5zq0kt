fbc79800d525ebbbfe2418763be1207bdb039812
harish242 <markollh242@gmail.com>
2023-06-09 10:04:59 +0530

    hg

--- src/components/ListItems.js
+++ src/components/ListItems.js
@@ -3,11 +3,16 @@ import React from "react";
 const ListItems = (props) => {
   const {list}=props
   return (
+    <div>
+        <div className="items">Hello, I'm the first child!</div>
   <ul>
+    
     {list.map(all=>{
     return  <li className="items">Hello the value is {all}</li>;
     })}
   </ul>
+    </div>
+    
   )
 };
 export default ListItems;
